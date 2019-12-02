4b67f452d4d93dfa0b01a8c996d9edcdc55491c7
jakemraz <jakemraz100@gmail.com>
2019-12-02 20:59:43 +0900

    add input receiver and sample code

--- sample_lambda_function.py
+++ sample_lambda_function.py
@@ -3,19 +3,20 @@ import json
 
 
 def lambda_handler(event, context):
+    # TODO implement
     output = []
-    
+    test_data = {
+        'name': 'jakemraz',
+        'age': 13
+    }
     for record in event['records']:
 
-        data = json.loads(base64.b64decode(record['data'].encode('utf-8')).decode('utf-8'))
         # transform here
-        new = data['event_type']
-
 
         out_record = {
             'recordId': record['recordId'],
             'result': 'Ok',
-            'data': base64.b64encode(json.dumps(new).encode('utf-8')).decode('utf-8')
+            'data': base64.b64encode(json.dumps(test_data).encode('utf-8')).decode('utf-8')
         }
         output.append(out_record)
     return {'records': output}
